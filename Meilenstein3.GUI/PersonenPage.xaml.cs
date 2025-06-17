4540cb88998436d833397dc3940c3541d79429dd
MarlonwehrDE <marlon@marlonwehr.de>
2025-06-17 22:26:33 +0200

    Update PersonenPage.xaml.cs

--- Meilenstein3.GUI/PersonenPage.xaml.cs
+++ Meilenstein3.GUI/PersonenPage.xaml.cs
@@ -1,7 +1,7 @@
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-
+using System.Linq;
 namespace Meilenstein3.GUI
 {
 
@@ -28,22 +28,30 @@ namespace Meilenstein3.GUI
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             ((MainWindow)Application.Current.MainWindow).MainFrame.Navigate(new PersonAnlegenPage(this));
-            
+
         }
 
-       private void Loeschen_Click(object sender, RoutedEventArgs e)
-    {
-    var button = sender as Button;
-    var person = button?.Tag as Meilenstein3.Person.Personen;
+        private void Loeschen_Click(object sender, RoutedEventArgs e)
+        {
 
-    if (person != null && PersonenListe.Contains(person))
-    {
-        PersonenListe.Remove(person);
+            // Den Button casten
+            var button = sender as Button;
 
-        // Nach dem Entfernen: Liste speichern
-        var linkedList = new LinkedList<Meilenstein3.Person.Personen>(PersonenListe);
-        Meilenstein3.Person.Personen.SpeicherePersonenListe(linkedList);
-    }
-    }
+            // Die gebundene Person aus dem Tag holen
+            var person = button?.Tag as Meilenstein3.Person.Personen;
+
+            // Wenn alles korrekt ist: aus der ObservableCollection entfernen
+            if (person != null && PersonenListe.Contains(person))
+            {
+                PersonenListe.Remove(person);
+                var linkedList = new LinkedList<Meilenstein3.Person.Personen>(PersonenListe);
+                Meilenstein3.Person.Personen.SpeicherePersonenListe(linkedList);
+
+
+
+
+            }
+
+        }
     }
 }
